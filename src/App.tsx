3f3d818dbe7c25289d350da4762b2deb9da4cf65
Abdullah Rehman <rehmanabdullah945@gmail.com>
2021-10-07 12:32:56 +0500

    change in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>This is the start of our crypto wall project</p>
+        <p>u fuckin fuckin </p>
       </header>
     </div>
   );
