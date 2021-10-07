6676fc7c981c9c1bc7dc128bfc16d83d622fe479
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-10-07 12:31:59 +0500

    InitialL Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>This is the start of our crypto wall project</p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>This is the start of our crypto wall project</p>
+				<p>Muhammad Asher</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
