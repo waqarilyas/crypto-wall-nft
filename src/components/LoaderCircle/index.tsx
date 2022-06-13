b7f3613fb26add973fb21a295ab4155ea58388c6
Muhammad Waqar <66460686+waqarilyas@users.noreply.github.com>
2022-06-13 21:40:51 +0500

    Delete src directory

--- src/components/LoaderCircle/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import cn from "classnames";
-import styles from "./LoaderCircle.module.scss";
-
-const Loader = ({ className }) => {
-  return <div className={cn(styles.loader, className)}></div>;
-};
-
-export default Loader;
